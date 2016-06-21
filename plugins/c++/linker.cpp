a597ac0c0b8a61d3fd88c9857ad2c68891755b14
Michał 'Griwes' Dominiak <griwes@griwes.info>
2016-06-21 21:08:54 +0300

    Don't forget about LDFLAGS.

--- plugins/c++/linker.cpp
+++ plugins/c++/linker.cpp
@@ -61,7 +61,7 @@ void reaver::despayre::cxx::_v1::cxx_linker::_build(reaver::despayre::_v1::conte
     }
 
     boost::filesystem::create_directories(out.parent_path());
-    std::vector<std::string> args = { "/bin/sh", "-c", "exec clang++ ${CXXFLAGS} -std=c++1z -o '" + out.string() + "' " + additional_flags + flags + input_paths };
+    std::vector<std::string> args = { "/bin/sh", "-c", "exec clang++ ${CXXFLAGS} ${LDFLAGS} -std=c++1z -o '" + out.string() + "' " + additional_flags + flags + input_paths };
 
     using namespace boost::process::initializers;
     boost::process::pipe p = boost::process::create_pipe();
